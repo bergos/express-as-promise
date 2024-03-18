5296c97a2a64864140d3a0984270f6bb8c8c398b
bergi <bergi@axolotlfarm.org>
2024-03-18 18:00:00 +0100

    feat: added WebSocket support

--- test/ExpressAsPromise.test.js
+++ test/ExpressAsPromise.test.js
@@ -1,6 +1,7 @@
-import { notStrictEqual, strictEqual } from 'node:assert'
+import { match, notStrictEqual, strictEqual } from 'node:assert'
 import { describe, it } from 'mocha'
 import fetch from 'node-fetch'
+import { WebSocket, WebSocketServer } from 'ws'
 import ExpressAsPromise from '../index.js'
 
 describe('ExpressAsPromise', () => {
@@ -22,6 +23,45 @@ describe('ExpressAsPromise', () => {
       const content = await res.text()
 
       strictEqual(content, 'Hello World!')
+
+      await server.stop()
+    })
+  })
+
+  describe('.wss', () => {
+    it('should be a WebSocketServer object', async () => {
+      const server = new ExpressAsPromise()
+
+      server.app.get('/', (req, res) => {
+        res.send('Hello World!')
+      })
+
+      await server.listen()
+
+      strictEqual(server.wss instanceof WebSocketServer, true)
+
+      await server.stop()
+    })
+
+    it('should be bound to the server socket', async () => {
+      const server = new ExpressAsPromise()
+
+      server.wss.on('connection', ws => {
+        ws.send('test')
+      })
+
+      const url = await server.listen()
+      const socket = new WebSocket(`ws://${url.host}/`)
+
+      await new Promise(resolve => {
+        socket.on('message', message => {
+          strictEqual(message.toString(), 'test')
+
+          socket.close()
+
+          resolve()
+        })
+      })
 
       await server.stop()
     })
@@ -96,12 +136,13 @@ describe('ExpressAsPromise', () => {
       await server.stop()
     })
 
-    it('should return the base URL', async () => {
+    it('should return the base URL as URL object', async () => {
       const server = new ExpressAsPromise()
 
       const baseUrl = await server.listen(12345, 'localhost')
 
-      strictEqual(baseUrl, 'http://127.0.0.1:12345/')
+      strictEqual(baseUrl instanceof URL, true)
+      match(baseUrl.toString(), /http:\/\/(127.0.0.1|\[::1\]):12345\//)
 
       await server.stop()
     })
@@ -120,17 +161,18 @@ describe('ExpressAsPromise', () => {
       await server.listen()
       await server.stop()
 
-      strictEqual(server.server, null)
+      strictEqual(server.server.listening, false)
     })
   })
 
   describe('.url', () => {
-    it('should be a readable property to get the base URL', async () => {
+    it('should be a readable URL property to get the base URL', async () => {
       const server = new ExpressAsPromise()
 
       await server.listen(12346, 'localhost')
 
-      strictEqual(server.url, 'http://127.0.0.1:12346/')
+      strictEqual(server.url instanceof URL, true)
+      match(server.url.toString(), /http:\/\/(127.0.0.1|\[::1\]):12346\//)
 
       await server.stop()
     })
@@ -146,7 +188,7 @@ describe('ExpressAsPromise', () => {
 
       await server.listen(12347, 'localhost')
 
-      strictEqual(server.url, 'http://127.0.0.1/')
+      match(server.url.toString(), /http:\/\/(127.0.0.1|\[::1\])\//)
 
       await server.stop()
     })
